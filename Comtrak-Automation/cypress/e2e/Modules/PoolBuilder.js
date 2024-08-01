64e66b0786b62a5ec43514e05fca87b314544e58
Aizaz <aizaz.sqa@gmail.com>
2024-08-01 16:28:15 +0500

    Changes

--- Comtrak-Automation/cypress/e2e/Modules/PoolBuilder.js
+++ Comtrak-Automation/cypress/e2e/Modules/PoolBuilder.js
@@ -1,6 +1,6 @@
 import PoolBuilderSelectors from '../../Selectors/PoolBuilderSelectors.js'
 
-const {} = PoolBuilderSelectors
+const { } = PoolBuilderSelectors
 
 class PoolBuilder {
   CreatePoolBuilder() {
@@ -9,66 +9,62 @@ class PoolBuilder {
 
       cy.visit('https://comtrak.qa.dmclinical.com/')
       cy.origin('https://comtrak.qa.dmclinical.com/', () => {
-        cy.wait(10000);
-        // open "leads" dropdown
+        cy.wait(10000)
         cy.contains('Sign In').click()
         // cy.xpath('//img/parent::button[@class="btn btndesk fontopenSans "]').click()
         cy.wait(5000)
-        
+
         cy.contains('Leads ').click();
         cy.wait(5000)
 
         // cy.xpath('//b/parent::p[text()="Leads "]').click()
 
-      // // // click on "patient pool Builder"
-      // cy.wait(2000);
-      cy.contains('Patient Pool Builder ').click()
-      cy.wait(5000)
-      
-      cy.contains(' Create New ').click()
-      cy.wait(2000)
-      // // create a New Patient Pool
-      // // Pool Name
-      // cy.get('input[placeholder="Enter Pool Name"]').type('Auto-test#123');
-      // // // Pool Description
-      // // cy.get('#poolDescription').type('testing#20')
-      // //click on "continue" Button
-      // cy.get('button[class="btn btn-primary"]').click({ force: true });
-      // // // DEFINE LIST
+        cy.contains('Patient Pool Builder ').click()
+        cy.wait(5000)
 
-      // // // Select State
-      // cy.get('span.ng-star-inserted').contains('Select State').click({ force: true });
-      // // cy.wait('2000')
-      // // cy.contains('New York').click({ force: true })
-      // cy.contains('New Jersey').click({ force: true });
-
-      // // // status
-      // cy.contains('Select Status').click({ force: true });
-      // // cy.wait('2000')
-      // //cy.contains('Deceased').click({ force: true })
-      // cy.contains('Pre Booking').click({ force: true });
+        cy.contains(' Create New ').click()
+        cy.wait(2000)
 
 
-      // // // The age range selection
-      // cy.get('.ngx-slider-pointer-min')
-      //   .focus()
-      //   .type('{rightArrow}'.repeat(6));
+        // cy.get('input[placeholder="Enter Pool Name"]').type('Auto-test#123');
 
-      // cy.get('.ngx-slider-pointer-max')
-      //   .focus()
-      //   .type('{leftArrow}'.repeat(2));
+        // // cy.get('#poolDescription').type('testing#20')
 
-      // // This is for Reset Criteria
-      // //cy.contains(' Reset Criteria ').click( {force:true} );
-      // //For create a pool click on continue
-      // cy.get('button[class="btn btn-primary btn-next"]').click({ force: true });
-      // //Selection in intended Study
-      // cy.wait(2000)
-      // cy.contains('Select Intended Study').click({ force: true });
-      // cy.contains('Moderna 1273-p301').click({ force: true });
-      // cy.get('button[class="btn btn-primary btn-sm btn-next"]').click({ force: true });
+        // cy.get('button[class="btn btn-primary"]').click({ force: true });
+
+
+        // cy.get('span.ng-star-inserted').contains('Select State').click({ force: true });
+        // // cy.wait('2000')
+        // // cy.contains('New York').click({ force: true })
+        // cy.contains('New Jersey').click({ force: true });
+
+
+        // cy.contains('Select Status').click({ force: true });
+        // // cy.wait('2000')
+        // //cy.contains('Deceased').click({ force: true })
+        // cy.contains('Pre Booking').click({ force: true });
+
+
+
+        // cy.get('.ngx-slider-pointer-min')
+        //   .focus()
+        //   .type('{rightArrow}'.repeat(6));
+
+        // cy.get('.ngx-slider-pointer-max')
+        //   .focus()
+        //   .type('{leftArrow}'.repeat(2));
+
+
+        // //cy.contains(' Reset Criteria ').click( {force:true} );
+
+        // cy.get('button[class="btn btn-primary btn-next"]').click({ force: true });
+
+        // cy.wait(2000)
+        // cy.contains('Select Intended Study').click({ force: true });
+        // cy.contains('Moderna 1273-p301').click({ force: true });
+        // cy.get('button[class="btn btn-primary btn-sm btn-next"]').click({ force: true });
       })
-      
+
     })
   }
 }
--- Comtrak-Automation/cypress/e2e/Modules/login.js
+++ Comtrak-Automation/cypress/e2e/Modules/login.js
@@ -1,23 +1,12 @@
 import LoginSelectors from '../../Selectors/LoginSelectors.js'
-
-const {TXT_Email, Email_Id, TXT_Password, Password_Id, BTN_Next, BTN_SignIn} = LoginSelectors
+const { TXT_Email, Email_Id, TXT_Password, Password_Id, BTN_Next, BTN_SignIn } = LoginSelectors
 
 class login {
-    //describe('ComTrak', () => {
-
-
-    // Use before() to run the login session setup once before all tests
-    //   before(() => {
-
     CreateSession() {
         beforeEach(() => {
-            // Create a new session with the provided email and password
             Cypress.session.clearAllSavedSessions()
             cy.session([Email_Id, Password_Id], () => {
-                // Visit the Google login page
                 cy.visit('https://accounts.google.com/v3/signin/identifier?opparams=%253Fopenid.realm%2526ss_domain%253Dhttps%25253A%25252F%25252Fcomtrak.dev.dmclinical.com&dsh=S286069110%3A1720167782606180&client_id=687199414693-c4ocf6bslimh2mfipbir5o6fiiofgrku.apps.googleusercontent.com&ddm=0&fetch_basic_profile=true&gsiwebsdk=2&include_granted_scopes=true&o2v=1&redirect_uri=storagerelay%3A%2F%2Fhttps%2Fcomtrak.dev.dmclinical.com%3Fid%3Dauth619290&response_type=permission+id_token&scope=email+profile+openid&service=lso&flowName=GeneralOAuthFlow&continue=https%3A%2F%2Faccounts.google.com%2Fsignin%2Foauth%2Fconsent%3Fauthuser%3Dunknown%26part%3DAJi8hAMl9akc_q4VsjE5nnptjJSk5s8J7evLmsFNQ3iKD3erZvsfwUjXUaszKeqCtxKp4vDnaXcs5KEHIjUA30u-RUnzjLhYYuLBc8TX3pK9LgMnpRuG5Y_zuKhlsg__417ii-AfKStMCnbesHz-uzGuL6jbaOlNdcInjOMZMVuT74tLOfCtRVrOBlNb2GvbhSGWQUcNwHADqzPY8eogoMG0rX0HgJWs-XC7LbVG07gKpFi242I1-g-HuBsacE2XAtiWMX3ttfga66hAKHyTqc4KS-jhJdZy74j_t4a8Bc4xsZ_0R_weKxYqDCJrv2ua7A7ESCL6gJYEru-uPMU5TyxvA0ASFGPviYYRMG23hZTL5OwWVTriUXyHALaVkB1f_zhv95JykJbQBJHcQVKOeEztTAB7yN1qPF4lC-VpbynGodyIvUFJNhtWgRGjhEkSh8P6bMwL1aBHmZtapmaW2tTKc8pGddEOfC_R4P_U9kuJcZ7oW7gmIwM%26flowName%3DGeneralOAuthFlow%26as%3DS286069110%253A1720167782606180%26client_id%3D687199414693-c4ocf6bslimh2mfipbir5o6fiiofgrku.apps.googleusercontent.com%23&app_domain=https%3A%2F%2Fcomtrak.dev.dmclinical.com&rart=ANgoxcfkiw_N3StLWSsM3R01-tzqzIICnpTdz2LxNof1ldDBXu2D3y2iAAw1TuIQk_BDrrJ384Y47LIfD4lWLzx-4L1m8kcYKiF9pVhP0AS4VmtSzo-TErs')
-
-                // Enter the email and password, then click 'Next'
                 cy.xpath(TXT_Email).type(Email_Id, { timeout: 10000 })
                 cy.xpath(BTN_Next).click()
                 cy.wait(2000)
@@ -36,32 +25,14 @@ class login {
             })
         })
         it('Login', () => {
-
-            // Visit the ComTrak login page
-            //cy.visit('https://comtrak.dev.dmclinical.com/');
-            cy.visit('https://comtrak.qa.dmclinical.com/');
-            // Use cy.origin to handle cross-origin navigation
+            cy.visit('https://comtrak.qa.dmclinical.com/')
             cy.origin('https://comtrak.qa.dmclinical.com/', () => {
-                // Wait for the page to load and click 'Sign In'
                 cy.wait(5000)
-                // cy.xpath('//img/parent::button[@class="btn btndesk fontopenSans "]').click();
+                // cy.xpath('//img/parent::button[@class="btn btndesk fontopenSans "]').click()
                 cy.contains('Sign In').click()
                 cy.wait(5000)
-                // cy.contains('exit_to_app').click()
-                // cy.wait(5000);
-                // click on sign in popup
-                // cy.contains('Sign In').click({ force: true });
-                // cy.wait(15000)
-//ul//li/child::a/child::p[text()="Leads "] Leads
-//b/parent::p[text()="Leads "]Leads
-//img/parent::button[@class="btn btndesk fontopenSans "] Sign in
-                /////////////////////////////////////////////////////////////////////////////////////////////
             });
         })
-    }
-
-    LoginSession() {  // Define the test case for logg ing in
-        
     }
 }
 
