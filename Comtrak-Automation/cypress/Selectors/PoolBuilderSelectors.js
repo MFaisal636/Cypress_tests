bae45bbf10e4c0585e7781d995cd5556a6454c4b
Aizaz <aizaz.sqa@gmail.com>
2024-08-01 18:53:59 +0500

    Changes

--- Comtrak-Automation/cypress/Selectors/PoolBuilderSelectors.js
+++ Comtrak-Automation/cypress/Selectors/PoolBuilderSelectors.js
@@ -1,8 +1,36 @@
 const PoolBuilderSelectors = {
 
     // Xpath Selectors
-    DRP_Leads : 'b/parent::p[text()="Leads "]',
+    TXT_PoolName: '//input[@id="poolName"]',
+    PoolName: 'Auto-QA Test ' + Math.floor(Math.random() * 1000),
+    TXT_Description: '//textarea[@id="poolDescription"]',
+    Description: 'Test Description',
+    BTN_Continue1: '(//button[text()=" Continue "])[1]',
+    DRP_State: '//span[text()="Select State"]',
+    STATE_Search: '(//input[@placeholder="Search"])[1]',
+    STATE_NewYork: '//input[@aria-label="New York"]',
+    STATE_NewJersey: '//input[@aria-label="New Jersey"]',
+    DRP_Status: '//span[text()="Select Status"]',
+    STATUS_Search: '(//input[@placeholder="Search"])[2]',
+    STATUS_PreBooking: '//input[@aria-label="Pre Booking"]',
+    MIN_AgeSlider: '//span[@aria-label="ngx-slider"]',
+    MAX_AgeSlider: '//span[@aria-label="ngx-slider-max"]',
+    BTN_Reset: '//button[text()=" Reset Criteria "]',
+    BTN_Continue2: '(//button[text()=" Continue "])[1]',
+    BTN_SaveDraft: '(//button[text()=" Save as draft "])[1]',
     
+    // CSS Selectors
+    TXT_PoolName: 'input[placeholder="Enter Pool Name"]',
+    PoolName: 'Auto-QA Test ' + Math.floor(Math.random() * 1000),
+    TXT_Description: '#poolDescription',
+    Description: 'Test Description',
+    BTN_Continue1: 'button[class="btn btn-primary"]',
+    DRP_State: 'span.ng-star-inserted',
+    DRP_Status: 'span.ng-star-inserted',
+    MIN_AgeSlider: '.ngx-slider-pointer-min',
+    MAX_AgeSlider: '.ngx-slider-pointer-max',
+    BTN_Continue2: 'button[class="btn btn-primary btn-next"]',
+
 
 }
 export default PoolBuilderSelectors
--- Comtrak-Automation/cypress/e2e/Modules/PoolBuilder.js
+++ Comtrak-Automation/cypress/e2e/Modules/PoolBuilder.js
@@ -1,6 +1,6 @@
 import PoolBuilderSelectors from '../../Selectors/PoolBuilderSelectors.js'
 
-const { } = PoolBuilderSelectors
+const {TXT_PoolName, PoolName, TXT_Description, Description, BTN_Continue1, DRP_State, STATE_Search, STATE_NewYork, STATE_NewJersey, DRP_Status, STATUS_Search, STATUS_PreBooking, MIN_AgeSlider, MAX_AgeSlider, BTN_Reset, BTN_Continue2, BTN_SaveDraft} = PoolBuilderSelectors
 
 class PoolBuilder {
   CreatePoolBuilder() {
@@ -61,43 +61,34 @@ class PoolBuilder {
 
         // Using CSS Selectors
 
-        cy.get('input[placeholder="Enter Pool Name"]').type('Auto-QA Test ' + Math.floor(Math.random() * 1000))
+        cy.get(TXT_PoolName).type(PoolName)
 
-        cy.get('#poolDescription').type('Test Description')
+        cy.get(TXT_Description).type(Description)
 
-        cy.get('button[class="btn btn-primary"]').click()
+        cy.get(BTN_Continue1).click()
         cy.wait(5000)
 
-
-        cy.get('span.ng-star-inserted').contains('Select State').click()
+        cy.get(DRP_State).contains('Select State').click()
         cy.wait(2000)
-        // cy.get('input[class="ng-valid ng-touched ng-dirty"]:nth-child(1)').click({force:true}).type('New')
         cy.contains('New York').click()
         cy.contains('New Jersey').click()
         cy.wait(2000)
 
-
-        cy.get('span.ng-star-inserted').contains('Select Status').click()
+        cy.get(DRP_Status).contains('Select Status').click()
         cy.wait(2000)
-        // cy.get('input[class="ng-valid ng-dirty ng-touched"]:nth-child(1)').click({force:true}).type('Pre')
-        // cy.contains('Deceased').click()
         cy.contains('Pre Booking').click()
         cy.wait(2000)
 
-
-
-        cy.get('.ngx-slider-pointer-min')
+        cy.get(MIN_AgeSlider)
           .focus()
           .type('{rightArrow}'.repeat(6))
 
-        cy.get('.ngx-slider-pointer-max')
+        cy.get(MAX_AgeSlider)
           .focus()
           .type('{leftArrow}'.repeat(2))
 
-
         // //cy.contains(' Reset Criteria ').click( {force:true} );
-
-        cy.get('button[class="btn btn-primary btn-next"]').click()
+        // cy.get(BTN_Continue2).click()
 
         cy.wait(2000)
         // cy.contains('Select Intended Study').click({ force: true })
